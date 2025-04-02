b8156e943abae24ec57230771ae12c5c43dea6db
ram <ramduth@docme.cloud>
2025-04-02 12:25:51 +0530

    Nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  const letters = "RAMDUTH ".split("");
+  const letters = "RAMDUTH. ".split("");
 
   const AnimatedIcon = ({ children }) => (
     <motion.div
